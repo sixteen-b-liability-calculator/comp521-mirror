70427fabc49bc495ed2e02d3813b08fca9e0f8d3
David Adler <d.adler.s@gmail.com>
2014-09-23 20:07:53 -0400

    more stuff.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -13,8 +13,8 @@ def name():
         return "input not json"
     if isinstance(input_data, dict):
         output = dict()
-        for (k,v) in input_data.():
-            output[k[::-1]] = v
+        for (k,v) in input_data.iteritems():
+            output[k[::-1] if isinstance(k,unicode) else k] = v
         return jsonify(output)
     return jsonify(input_data)
 
