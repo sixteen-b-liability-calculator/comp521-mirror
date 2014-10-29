0affcf9bca09eaf9c379a39e5f64e09c3149d004
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-10-29 11:49:12 -0400

    Added e-mail functionality.  Add a "recepient" value in the dict you send to compute in order to decide who to sent the result to.

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
+from flask_mail import Message, Mail
 import sys
 import os
 from coopr.pyomo import *
@@ -6,12 +7,22 @@ from coopr.opt import SolverFactory
 import coopr.environ
 
 app = Flask(__name__, static_url_path='')
+app.config.update(dict(
+    DEBUG = False,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'kevin.valakuzhy@gmail.com',
+    MAIL_PASSWORD = 'Doingthem0st',
+))
+mail = Mail(app)
 
 from compute import run_problem, validate_buysell, FourhundredException
 
 @app.route("/", methods = ['GET'])
 def homepage():
-    return app.send_static_file('index.html')
+	return ("Whoopsie", 400, [])
 
 @app.route("/compute", methods=['POST'])
 def compute_endpoint():
@@ -26,6 +37,7 @@ def compute_endpoint():
     try:
         purchases = validate_buysell('buy', input_data.get('buy'))
         sales = validate_buysell('sell', input_data.get('sell'))
+        recipient = input_data.get('recipient')
     except FourhundredException as e:
         return (e.msg, 400, [])
 
@@ -37,7 +49,9 @@ def compute_endpoint():
 
     if not app.debug:
         del result['full_result']
-
+    if (recipient != None):    
+	    msg = Message(subject = "Test e-mail", body =str(result), sender="kevin.valakuzhy@gmail.com", recipients=[recipient])
+	    mail.send(msg)
     return jsonify(result)
 
 if __name__ == "__main__":
