e7d028f81df3e26efeb3069f3052f28b0a705216
David Adler <d.adler.s@gmail.com>
2014-11-05 00:05:18 -0500

    Add ungzip_tempfile and parse_idx.

--- wsgi/edgar_api.py
+++ wsgi/edgar_api.py
@@ -1,6 +1,9 @@
 from ftplib import FTP
 import tempfile
 
+import re
+import gzip
+
 def homepage():
 	return ("Whoopsie", 400, [])
 
@@ -24,3 +27,33 @@ def pull_index_Edgar():
 	ftp.retrbinary('RETR '+ indexType + '.gz', pracFile.write)
 	pracFile.seek(0)
 	return pracFile
+
+parse_idx_start = re.compile(r'-+$')
+parse_idx_entry = re.compile(r''' ( [0-9]  + )    # CIK
+                               \|   [^|]   *      # (skip) name
+                               \| ( [^|]   + )    # form type
+                               \|   [0-9-] +      # (skip) date
+                               \| ( edgar/data/[0-9]+/[0-9-]+\.txt ) # path
+                               $ ''', re.VERBOSE)
+
+def ungzip_tempfile(fileobj):
+    fileobj.seek(0)
+    return gzip.GzipFile("", fileobj=fileobj, mode='r')
+
+def parse_idx(fileobj, target_cik, form_types):
+    out = []
+    found_start = False
+    for line in fileobj:
+        if found_start: # i.e., we're looking at real entries now
+            m = parse_idx_entry.match(line.strip())
+            if not m:
+                # we expect to be able to parse every line after the start
+                raise Exception("could not parse line from EDGAR idx:", line)
+            if int(m.group(1)) == target_cik and m.group(2) in form_types:
+                out.append(m.group(3))
+        elif parse_idx_start.match(line.strip()):
+            found_start = True
+    if not found_start:
+        raise Exception("could not find start line in EDGAR idx")
+
+    return out
