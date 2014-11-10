ee150808d63c88c1e414d22b778c813cf69e8c40
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-11-09 23:03:21 -0500

    Added function for parsing inputs from section 4 files

--- wsgi/edgar_api.py
+++ wsgi/edgar_api.py
@@ -1,5 +1,9 @@
 from ftplib import FTP
+from datetime import datetime
+import xml.etree.ElementTree as ET
 import tempfile
+
+
 
 import re
 import gzip
@@ -19,13 +23,16 @@ def pull_index_Edgar():
         return (e.msg, 400, [])
 
 	ftp = FTP('ftp.sec.gov')
-	ftp.login()
-	directoryPath = 'edgar/'+year+'/QTR'+quarter+'/'+indexType+'.gz'
-	ftp.cwd(directoryPath)
+    pracFile = tempfile.TemporaryFile()
+    try: 
+    	ftp.login()
+    	directoryPath = 'edgar/'+year+'/QTR'+quarter+'/'+indexType+'.gz'
+    	ftp.cwd(directoryPath)
 
-	pracFile = tempfile.TemporaryFile()
-	ftp.retrbinary('RETR '+ indexType + '.gz', pracFile.write)
-	pracFile.seek(0)
+    	ftp.retrbinary('RETR '+ indexType + '.gz', pracFile.write)
+    	pracFile.seek(0)
+    finally:
+        ftp.close()
 	return pracFile
 
 parse_idx_start = re.compile(r'-+$')
@@ -57,3 +64,48 @@ def parse_idx(fileobj, target_cik, form_types):
         raise Exception("could not find start line in EDGAR idx")
 
     return out
+
+# Parses a file containing text from the edgar database.  Returns a tree containing
+# all the information that you need
+# N.B. Assumes that the file contains valid xml within <XML> tags  
+def parse_section_4(inputFile):
+    # Find the beginning of the xml in the file
+    while not inputFile.readline().startswith('<XML'):
+        pass
+
+    # Start writing these lines into a separate file in order to remove lines at the bottom
+    xmlFile = tempfile.TemporaryFile()
+    
+    while True:
+        line = inputFile.readline()
+        if line.startswith('</XML'):
+            break
+        else:
+            xmlFile.write(line)
+    inputFile.close() 
+    # Turn the file into a tree structure
+    xmlFile.seek(0)
+    tree = ET.parse(xmlFile)
+    xmlFile.close()
+    return tree
+
+# Parses the tree to gain the information needed to return the proper object
+# Right now : only includes non-derivative transactions
+def return_trade_information_from_xml(tree):
+    buy = []
+    sell = []
+    for node in tree.iter('nonDerivativeTransaction'):
+        shares = float(node.find('.//transactionShares/value').text)
+        date = node.find('.//transactionDate/value').text
+        pricePerShare = float(node.find('.//transactionPricePerShare/value').text)
+        BuyOrSell = node.find('.//transactionAcquiredDisposedCode/value').text
+
+        # Parse date to get day month and year
+        dateElement = datetime.strptime(date,'%Y-%m-%d')
+
+        if (BuyOrSell == 'D'): # Implies sell
+            buy.append(dict(number=shares, price=pricePerShare, year=dateElement.year, month=dateElement.month, day=dateElement.day))
+        elif (BuyOrSell == 'A'): # Implies buy
+            sell.append(dict(number=shares, price=pricePerShare, year=dateElement.year, month=dateElement.month, day=dateElement.day))
+
+    return [buy,sell]
--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -2,12 +2,25 @@ import os
 import myflaskapp
 import unittest
 import tempfile
-
+from edgar_api import *
 class FlaskrTestCase(unittest.TestCase):
 
-    def test_empty_db(self):
-        rv = self.get('/')
-        assert 'No entries here so far' in rv.data
+    def test_blank(self):
+        assert 1==1
+
+    def test_parse_section_4(self):
+
+    	expectedBuy = dict(price = 44.10, month = 1, number = 2000, day = 11, year = 2007)
+    	expectedSell = dict(price= 34.585, month= 1, number= 10000, day= 11, year= 2007)
+
+        inputFile = open('edgarTestingFile.txt', 'r+')
+        tree = parse_section_4(inputFile)
+        assert tree.getroot().tag == 'ownershipDocument'
+        trades = return_trade_information_from_xml(tree)
+
+        assert trades[0][0] == expectedBuy
+        assert trades[1][0] == expectedSell  
+
 
 if __name__ == '__main__':
     unittest.main()
