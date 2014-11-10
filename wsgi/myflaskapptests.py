950c55564f213934b751d756d5b11dd8565d7406
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-11-10 01:00:04 -0500

    Additional tests for index and file parsing

--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -13,7 +13,7 @@ class FlaskrTestCase(unittest.TestCase):
     	expectedBuy = dict(price = 44.10, month = 1, number = 2000, day = 11, year = 2007)
     	expectedSell = dict(price= 34.585, month= 1, number= 10000, day= 11, year= 2007)
 
-        inputFile = open('edgarTestingFile.txt', 'r+')
+        inputFile = open('testing/edgarTestingFile.txt', 'r+')
         tree = parse_section_4(inputFile)
         assert tree.getroot().tag == 'ownershipDocument'
         trades = return_trade_information_from_xml(tree)
@@ -21,6 +21,12 @@ class FlaskrTestCase(unittest.TestCase):
         assert trades[0][0] == expectedBuy
         assert trades[1][0] == expectedSell  
 
+    def test_parse_idx(self):
+
+    	inputFile = open('testing/edgarTestIndex.txt', 'r+')
+    	edgarFileURLs = parse_idx(inputFile, 1000180, ['4'])
+    	assert edgarFileURLs[0] == 'edgar/data/1000180/0001242648-07-000020.txt'
+    	assert edgarFileURLs[9] == 'edgar/data/1000180/0001242648-07-000029.txt'
 
 if __name__ == '__main__':
     unittest.main()
