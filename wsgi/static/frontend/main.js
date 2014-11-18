c7fed6727418079ba2f4fd30992268029a233696
illestrater <Tim@dhcp-v880-13-00621.1xwireless.unc.edu>
2014-11-18 18:59:32 -0500

    Fixing json output reading

--- wsgi/static/frontend/main.js
+++ wsgi/static/frontend/main.js
@@ -134,21 +134,25 @@ function printOutput(data){
     var pairingsRow = 0;
     var maxprofit = 0;
     
+    {"pairs":[{"buy": {"day": 29,"month": 10,"number": 28,"price": 1879,"year": 5},"count": 10,"sell": {"day": 14,"month": 12,"number": 10,"price": 9872,"year": 5}},{"buy": {"day": 3,"month": 5,"number": 29,"price": 109,"year": 5},"count": 23,"sell": {"day": 8,"month": 8,"number": 23,"price": 1987,"year": 5}},{"buy": {"day": 3,"month": 5,"number": 29,"price": 109,"year": 5},"count": 6,"sell": {"day": 29,"month": 6,"number": 29,"price": 1827,"year": 5}}],"status": "optimal","value": 133432}
+    
+    {"pairs":[{"buy":{"day":3,"month":4,"number":6,"price":10,"year":5},"count":6,"sell":{"day":5,"month":5,"number":200,"price":1029,"year":5}},{"buy":{"day":3,"month":4,"number":30,"price":500,"year":5},"count":30,"sell":{"day":5,"month":5,"number":200,"price":1029,"year":5}},{"buy":{"day":3,"month":4,"number":8,"price":20,"year":5},"count":8,"sell":{"day":5,"month":5,"number":200,"price":1029,"year":5}}],"status":"optimal","value":30056}
+    
     for(i = 0; i < testData.length; i++){
     	if(testData[i] == "buy"){
-	    	pday = testData[i+3].substring(0, testData[i+3].length-1).substring(2);
-	    	pmonth = testData[i+5].substring(0, testData[i+5].length-1).substring(2);
-	    	pnumber = testData[i+7].substring(0, testData[i+7].length-1).substring(2);
-	    	pprice = testData[i+9].substring(0, testData[i+9].length-1).substring(2);
-	    	pyear = testData[i+11].substring(0, testData[i+11].length-2).substring(2);
+	    	pday = testData[i+3].substring(0, testData[i+3].length-1).substring(1);
+	    	pmonth = testData[i+5].substring(0, testData[i+5].length-1).substring(1);
+	    	pnumber = testData[i+7].substring(0, testData[i+7].length-1).substring(1);
+	    	pprice = testData[i+9].substring(0, testData[i+9].length-1).substring(1);
+	    	pyear = testData[i+11].substring(0, testData[i+11].length-2).substring(1);
 	    	
-	    	count = testData[i+13].substring(0, testData[i+13].length-1).substring(2);
+	    	count = testData[i+13].substring(0, testData[i+13].length-1).substring(1);
 	    	
-	    	sday = testData[i+17].substring(0, testData[i+17].length-1).substring(2);
-	    	smonth = testData[i+19].substring(0, testData[i+19].length-1).substring(2);
-	    	snumber = testData[i+21].substring(0, testData[i+21].length-1).substring(2);
-	    	sprice = testData[i+23].substring(0, testData[i+23].length-1).substring(2);
-	    	syear = testData[i+25].substring(0, testData[i+25].length-4).substring(2);
+	    	sday = testData[i+17].substring(0, testData[i+17].length-1).substring(1);
+	    	smonth = testData[i+19].substring(0, testData[i+19].length-1).substring(1);
+	    	snumber = testData[i+21].substring(0, testData[i+21].length-1).substring(1);
+	    	sprice = testData[i+23].substring(0, testData[i+23].length-1).substring(1);
+	    	syear = testData[i+25].substring(0, testData[i+25].length-4).substring(1);
 	    	
 	    	pairingsRow++;
 			row = table.insertRow(pairingsRow);
@@ -191,8 +195,6 @@ function printOutput(data){
     cell.innerHTML = '<strong>Total</strong>';
     cell = row.insertCell(5);
     cell.innerHTML = '$' + maxprofit;
-    
-    document.getElementById("save").style.display = 'block';
     
     // <form action="save.php" method="post" id="save"><input type="submit" class="btn btn-default col-md-6" value="Save Data"></form>
 }
