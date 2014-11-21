83125ff2afacce0e299558b75947b582b12cd376
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-11-21 18:04:09 -0500

    Cleaned up some code.

--- wsgi/static/frontend/main.js
+++ wsgi/static/frontend/main.js
@@ -92,11 +92,12 @@ function inputToJSON(){
     }
     
     email = $("#email").val()
-    
+
     $.ajax( "/compute",
 	({type: "POST",
 	    data: $.toJSON({ "buy": purchases, "sell": sales, "recipient": email }),
 	    contentType: "application/json",
+        dataType: "json",
 	    success: printOutput,
 	    error: function(data) {
 		    document.open();
@@ -110,56 +111,43 @@ function inputToJSON(){
 
 function printOutput(data){
 	
-	testData = JSON.stringify(data).split("\"");
-    var pday, pmonth, pnumber, pprice, pyear, sday, smonth, snumber, sprice, syear, count;
+    var pairs, pair, buy, sell, count;
     table = document.getElementById("pairings");
     $("#pairings tr:gt(0)").remove();
     var pairingsRow = 0;
     var maxprofit = 0;
     
     //{"pairs":[{"buy": {"day": 29,"month": 10,"number": 28,"price": 1879,"year": 5},"count": 10,"sell": {"day": 14,"month": 12,"number": 10,"price": 9872,"year": 5}},{"buy": {"day": 3,"month": 5,"number": 29,"price": 109,"year": 5},"count": 23,"sell": {"day": 8,"month": 8,"number": 23,"price": 1987,"year": 5}},{"buy": {"day": 3,"month": 5,"number": 29,"price": 109,"year": 5},"count": 6,"sell": {"day": 29,"month": 6,"number": 29,"price": 1827,"year": 5}}],"status": "optimal","value": 133432}
-    
     //{"pairs":[{"buy":{"day":3,"month":4,"number":6,"price":10,"year":5},"count":6,"sell":{"day":5,"month":5,"number":200,"price":1029,"year":5}},{"buy":{"day":3,"month":4,"number":30,"price":500,"year":5},"count":30,"sell":{"day":5,"month":5,"number":200,"price":1029,"year":5}},{"buy":{"day":3,"month":4,"number":8,"price":20,"year":5},"count":8,"sell":{"day":5,"month":5,"number":200,"price":1029,"year":5}}],"status":"optimal","value":30056}
-    
-    for(i = 0; i < testData.length; i++){
-    	if(testData[i] == "buy"){
-	    	pday = testData[i+3].substring(0, testData[i+3].length-1).substring(1);
-	    	pmonth = testData[i+5].substring(0, testData[i+5].length-1).substring(1);
-	    	pnumber = testData[i+7].substring(0, testData[i+7].length-1).substring(1);
-	    	pprice = testData[i+9].substring(0, testData[i+9].length-1).substring(1);
-	    	pyear = testData[i+11].substring(0, testData[i+11].length-2).substring(1);
-	    	
-	    	count = testData[i+13].substring(0, testData[i+13].length-1).substring(1);
-	    	
-	    	sday = testData[i+17].substring(0, testData[i+17].length-1).substring(1);
-	    	smonth = testData[i+19].substring(0, testData[i+19].length-1).substring(1);
-	    	snumber = testData[i+21].substring(0, testData[i+21].length-1).substring(1);
-	    	sprice = testData[i+23].substring(0, testData[i+23].length-1).substring(1);
-	    	syear = testData[i+25].substring(0, testData[i+25].length-4).substring(1);
-	    	
-	    	pairingsRow++;
-			row = table.insertRow(pairingsRow);
-			cell = row.insertCell(0);
-			cell.innerHTML = pmonth + '/' + pday + '/' + pyear;
-			cell.className = 'col-md-1';
-			cell = row.insertCell(1);
-			cell.innerHTML = '$' + pprice;
-			cell.className = 'col-md-1';
-			cell = row.insertCell(2);
-			cell.innerHTML = smonth + '/' + sday + '/' + syear;
-			cell.className = 'col-md-1';
-			cell = row.insertCell(3);
-			cell.innerHTML = '$' + sprice;
-			cell.className = 'col-md-1';
-			cell = row.insertCell(4);
-			cell.innerHTML = count;
-			cell.className = 'col-md-1';
-			cell = row.insertCell(5);
-			var profit = count*sprice - count*pprice;
-			cell.innerHTML = '$' + profit;
-			cell.className = 'col-md-1';
-			maxprofit += profit;
-    	}
+    pairs = data["pairs"]
+
+    for(var pairIdx in pairs){
+    	pair = pairs[pairIdx];
+        buy = pair["buy"];
+        sell = pair["sell"];
+    	
+    	pairingsRow++;
+		row = table.insertRow(pairingsRow);
+		cell = row.insertCell(0);
+		cell.innerHTML = buy["month"] + '/' + buy["day"] + '/' + buy["year"];
+		cell.className = 'col-md-1';
+		cell = row.insertCell(1);
+		cell.innerHTML = '$' + buy["price"];
+		cell.className = 'col-md-1';
+		cell = row.insertCell(2);
+		cell.innerHTML = sell["month"] + '/' + sell["day"] + '/' + sell["year"];
+		cell.className = 'col-md-1';
+		cell = row.insertCell(3);
+		cell.innerHTML = '$' + sell["price"];
+		cell.className = 'col-md-1';
+		cell = row.insertCell(4);
+		cell.innerHTML = pair["count"];
+		cell.className = 'col-md-1';
+		cell = row.insertCell(5);
+		var profit = pair["count"]*sell["price"] - pair["count"]*buy["price"];
+		cell.innerHTML = '$' + profit;
+		cell.className = 'col-md-1';
+		maxprofit += profit;
     }
     // Add summation line
     row = table.insertRow(pairingsRow+1);
@@ -214,8 +202,6 @@ function populate(data){
     $("#sales tr:gt(0)").remove();
     
 //  var beforeJSON = '{"buys":[{"day":11,"month":1,"number":2000,"price":44.1,"year":2007},{"day":11,"month":1,"number":1200,"price":44.39,"year":2007},{"day":11,"month":1,"number":3600,"price":44.76,"year":2007},{"day":11,"month":1,"number":2500,"price":45.04,"year":2007},{"day":11,"month":1,"number":700,"price":45.31,"year":2007},{"day":9,"month":2,"number":2000,"price":40.2,"year":2007},{"day":9,"month":2,"number":750,"price":40.6,"year":2007},{"day":23,"month":9,"number":15730,"price":54.84,"year":2006}],"sells":[{"day":11,"month":1,"number":10000,"price":34.585,"year":2007},{"day":9,"month":2,"number":5000,"price":3.125,"year":2007},{"day":9,"month":3,"number":5000,"price":2.5,"year":2007}]}';
-    
-    var month,day,year,number,price;
     
     var buys = data["buys"];
     var purchaseTable = $("#purchases")[0]
