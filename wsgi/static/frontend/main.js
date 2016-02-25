9f6093f8bb4b5f113c811e8fc0315ff0475bfdbf
Kaja Coraor <coraor@Kajas-MacBook-Air.local>
2016-02-24 22:48:35 -0500

    commenting

--- wsgi/static/frontend/main.js
+++ wsgi/static/frontend/main.js
@@ -301,6 +301,7 @@ function readTable(table){
 
 // Calculates max profit with linear programming or LIHO
 function inputToJSON(url){
+    // clear output tab
     $("#pairings").remove();
 
     if (inputHasErrors()) return;
