7cf534045980205a5daf1d397499a240f39c6beb
Kaja Coraor <coraor@dhcp-v80-101223.wireless.unc.edu>
2016-02-26 10:46:35 -0500

    add data to array instead of text

--- wsgi/static/frontend/main.js
+++ wsgi/static/frontend/main.js
@@ -575,7 +575,7 @@ function downloadOutput() {
         outputRow = new Array();
         $(this).find("td").each(function(i) {
             // console.log("this.firstChild: ", this.firstChild);
-            outputRow.push(this.firstChild);
+            outputRow.push(this.firstChild.data);
         });
         output.push(outputRow);
     });
