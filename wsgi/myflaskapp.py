074101d4285c5e485e72e8ca1382f0434390e9ad
Kaja Coraor <coraor@Kajas-MacBook-Air.local>
2016-04-22 19:04:41 -0400

    debugging select date functionality

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -166,7 +166,7 @@ def testDB():
 @app.route("/queryDB", methods=['GET'])
 @add_response_headers({'Access-Control-Allow-Origin': 'example.com'})
 def queryDB():
-    test = datetime.strftime(datetime.datetime.now()-datetime.timedelta(1),'%Y-%m-%d')
+    test = datetime.strftime(datetime.now()-timedelta(1),'%Y-%m-%d')
     print("****** TESTING: " + str(test))
     yesterday = str(datetime.now())
     yesterdayFormatted = yesterday[0:10]
