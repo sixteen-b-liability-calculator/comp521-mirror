4f75aaf9025caf733fadc41934e3f4b43016541d
Kaja Coraor <coraor@dhcp-v80-101223.wireless.unc.edu>
2016-04-24 15:18:04 -0400

    trying mysql connection from daily_report.py

--- wsgi/daily_report.py
+++ wsgi/daily_report.py
@@ -2,11 +2,19 @@ from edgar_api import pull_daily_filings, pull_trades
 from compute import run_problem
 from flask import jsonify
 from datetime import date, datetime, timedelta
+from myflaskapp import app, mysql
 import json
 
 # Get most recent Form 4 filings from EDGAR and update liability
 # database with new liability estimates for all filers
 def generate_daily_report(inputDate):
+
+    # connect to mysql
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.close() 
+    conn.close()  
+
     # Get information for yesterday's filings
     filings = []
 
@@ -37,5 +45,35 @@ def generate_daily_report(inputDate):
 
             # also add the filing date while we're at it
             filings[idx]['lastfiling'] = reportDate.strftime('%m/%d/%Y')
+
+
+
+# try:
+#     # read the posted values from the UI
+#     form = request.form
+#     _cik = request.form['inputCIK']
+#     _name = request.form['inputName']
+#     _lp = request.form['inputLP']
+#     _liho = request.form['inputLIHO']
+#     # validate the received values
+#     if _cik and _name and _lp and _liho:
+#         conn = mysql.connect()
+#         cursor = conn.cursor()
+#         cursor.callproc('add_person',(_cik, _name, _lp, _liho))
+#         data = cursor.fetchall()
+#         if len(data) is 0:
+#             conn.commit()
+#             return json.dumps({'message':'Person added successfully !'})
+#         else:
+#             return json.dumps({'error':str(data[0])})
+#     else:
+#         return json.dumps({'html':'<span>Enter the required fields</span>'})
+# except Exception as e:
+#     return json.dumps({'error':str(e)})
+# finally:
+#     cursor.close() 
+#     conn.close()
+
+
 
     return {'filings': filings}
