c525aa195e345f85a9df97a5d58e2709d7a6c8d5
Kaja Coraor <coraor@dhcp-v80-101223.wireless.unc.edu>
2016-04-24 16:09:11 -0400

    adding flask-mysql to install_requires

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='sixteen-b-liability-calculator',
       author='',
       author_email='',
       url='',
-      install_requires=['Flask>=0.7.2', 'Flask_mail', 'MarkupSafe', 'Coopr==4.0.9597', 'flask.ext.mysql'],
+      install_requires=['Flask>=0.7.2', 'Flask_mail', 'MarkupSafe', 'Coopr==4.0.9597', 'MySQL'],
      )
