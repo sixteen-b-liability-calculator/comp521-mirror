bedb34e6370ea298b7e58bfc10a3603224825e35
Kaja Coraor <coraor@dhcp-v80-101223.wireless.unc.edu>
2016-04-24 17:55:17 -0400

    Make link open in a new window

--- wsgi/static/frontend/main.js
+++ wsgi/static/frontend/main.js
@@ -31,7 +31,7 @@ $(document).ready( function () {
                     { title: "Date of most recent form 4/4A" },
                     { title: "URL of most recent form 4/4A",
                         "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                            $(nTd).html("<a href='"+oData[4]+"'>Link to filing</a>");
+                            $(nTd).html("<a href='"+oData[4]+" target=_blank'>Link to filing</a>");
                         }
                     }
                 ],
