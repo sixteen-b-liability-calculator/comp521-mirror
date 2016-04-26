631a6f1b3c718111368342c8735fa5b10bcfafcb
Kaja Coraor <coraor@Kajas-MacBook-Air.local>
2016-04-26 18:50:35 -0400

    debugging date function

--- wsgi/static/frontend/main.js
+++ wsgi/static/frontend/main.js
@@ -70,20 +70,21 @@ $(document).ready( function () {
                                 var secJSON = '{ "startYear":'+startYear+',"startMonth":'+startMonth+',"endYear":'+endYear+',"endMonth":'+endMonth+',"cik": "'+1194358+'"}';
 
                                 console.log("secJSON: " + secJSON);
-                                // if (secCIK && secCIK != "") {
-                                //     $.ajax( "/pullSEC",
-                                //         ({type: "POST",
-                                //         data: secJSON,
-                                //         contentType: "application/json",
-                                //         dataType: "json",
-                                //         success: [populate, removeMessage],
-                                //         error: function(data) {
-                                //             document.open();
-                                //             document.write(data.responseText);
-                                //             document.close();
-                                //         }
-                                //     }));
-                                // }
+                                
+                                if (secCIK && secCIK != "") {
+                                    $.ajax( "/pullSEC",
+                                        ({type: "POST",
+                                        data: secJSON,
+                                        contentType: "application/json",
+                                        dataType: "json",
+                                        success: [populate, removeMessage],
+                                        error: function(data) {
+                                            document.open();
+                                            document.write(data.responseText);
+                                            document.close();
+                                        }
+                                    }));
+                                }
                             });
                         }
                     },
