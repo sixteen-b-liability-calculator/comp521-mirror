6e26ea3567809706f8df3e7f287762f6cc7bd777
Alexandra Harrison <apharri3@live.unc.edu>
2016-04-26 19:14:41 -0400

    commenting out an old test

--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -26,12 +26,12 @@ class FlaskrTestCase(unittest.TestCase):
         assert trades[0][0] == expectedBuy
         assert trades[1][0] == expectedSell  
 
-    def test_parse_idx(self):
+    # def test_parse_idx(self):
 
-    	inputFile = open('testing/edgarTestIndex.txt', 'r+')
-    	edgarFileURLs = parse_idx(inputFile, 1000180, ['4'])
-    	assert edgarFileURLs[0] == 'edgar/data/1000180/0001242648-07-000020.txt'
-    	assert edgarFileURLs[9] == 'edgar/data/1000180/0001242648-07-000029.txt'
+    # 	inputFile = open('testing/edgarTestIndex.txt', 'r+')
+    # 	edgarFileURLs = parse_idx(inputFile, 1000180, ['4'])
+    # 	assert edgarFileURLs[0] == 'edgar/data/1000180/0001242648-07-000020.txt'
+    # 	assert edgarFileURLs[9] == 'edgar/data/1000180/0001242648-07-000029.txt'
 
     # This test can be flaky depending on the connection to the SEC database
     def test_pull_trades(self):
@@ -39,6 +39,12 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.post('/pullSEC', content_type= 'application/json', data = jsonData)
         data = json.loads(rv.get_data())
         assert data['sells'][0] == {"day": 11,"month": 1,"number": 2000,"price": 44.1, "year": 2007, "securityTitle":"Common Stock", "directOrIndirectOwnership" : "D", "filingURL" : "http://www.sec.gov/Archives/edgar/data/1000180/000124264807000001/0001242648-07-000001-index.htm"}
+
+    def test_pull_daily_filings(self):
+        jsonData = json.dumos({"dateString": 01/01/16})
+        rv = self.app.post('/ppullDailyReport', content_type = 'application/json', data = jsonData)
+        data = json.loads(rv.get_data())
+        assert data['sells'][0] == {"day": 11, "month": 1, "number": 2000, "price": 44.1, "year": 2007, "securityTitle":"Common Stock", "directOrIndirectOwnership" : "D", "filingURL" : "http://www.sec.gov/Archives/edgar/data/1000180/000124264807000001/0001242648-07-000001-index.htm"}
 
     # Testing the ability to pull files locally
     def test_pull_index_local(self):
