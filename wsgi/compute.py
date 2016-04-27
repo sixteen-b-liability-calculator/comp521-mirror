913606e8323b1416d980a7e4f6096faad2d83d66
madipfaff <mpfaff@live.unc.edu>
2016-04-26 21:02:21 -0400

    try graph #1

--- wsgi/compute.py
+++ wsgi/compute.py
@@ -356,5 +356,5 @@ def makeGraph(sale_dates, purchase_dates, sale_amounts, purchase_amounts):
     fig.autofmt_xdate()
     plt.show()
     #fig.savefig('fig.png')
-    ax.savefig('ax.png')
-    plt.savefig('plt.png')
+    #ax.savefig('ax.png')
+    #plt.savefig('plt.png')
