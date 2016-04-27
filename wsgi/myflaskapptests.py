c357460a3714b8b57e21e035b1c3546f7e88082e
Alexandra Harrison <apharri3@live.unc.edu>
2016-04-26 20:46:59 -0400

    putting all the tests back

--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -12,26 +12,26 @@ class FlaskrTestCase(unittest.TestCase):
     def test_blank(self):
         assert 1==1
 
-    # def test_parse_section_4(self):
+    def test_parse_section_4(self):
 
-    # 	expectedSell = dict(price = 44.10, month = 1, number = 2000, day = 11, year = 2007,
-    #                        securityTitle="Common Stock", directOrIndirectOwnership="D", filingURL ="ftp://ftp.sec.gov/")
-    # 	expectedBuy = dict(price= 34.585, month= 1, number= 10000, day= 11, year= 2007,
-    #                         securityTitle="Common Stock", directOrIndirectOwnership="D", filingURL ="ftp://ftp.sec.gov/")
+    	expectedSell = dict(price = 44.10, month = 1, number = 2000, day = 11, year = 2007,
+                           securityTitle="Common Stock", directOrIndirectOwnership="D", filingURL ="ftp://ftp.sec.gov/")
+    	expectedBuy = dict(price= 34.585, month= 1, number= 10000, day= 11, year= 2007,
+                            securityTitle="Common Stock", directOrIndirectOwnership="D", filingURL ="ftp://ftp.sec.gov/")
 
-    #     inputFile = open('wsgi/testing/edgarTestingFile.txt', 'r+')
-    #     tree = parse_section_4(inputFile)
-    #     assert tree.getroot().tag == 'ownershipDocument'
-    #     trades = return_trade_information_from_xml(tree,"")
-    #     assert trades[0][0] == expectedBuy
-    #     assert trades[1][0] == expectedSell  
+        inputFile = open('wsgi/testing/edgarTestingFile.txt', 'r+')
+        tree = parse_section_4(inputFile)
+        assert tree.getroot().tag == 'ownershipDocument'
+        trades = return_trade_information_from_xml(tree,"")
+        assert trades[0][0] == expectedBuy
+        assert trades[1][0] == expectedSell  
 
-    # def test_parse_idx(self):
+    def test_parse_idx(self):
 
-    # 	inputFile = open('testing/edgarTestIndex.txt', 'r+')
-    # 	edgarFileURLs = parse_idx(inputFile, 1000180, ['4'])
-    # 	assert edgarFileURLs[0] == 'edgar/data/1000180/0001242648-07-000020.txt'
-    # 	assert edgarFileURLs[9] == 'edgar/data/1000180/0001242648-07-000029.txt'
+    	inputFile = open('testing/edgarTestIndex.txt', 'r+')
+    	edgarFileURLs = parse_idx(inputFile, 1000180, ['4'])
+    	assert edgarFileURLs[0] == 'edgar/data/1000180/0001242648-07-000020.txt'
+    	assert edgarFileURLs[9] == 'edgar/data/1000180/0001242648-07-000029.txt'
 
     # This test can be flaky depending on the connection to the SEC database
 
@@ -50,28 +50,28 @@ class FlaskrTestCase(unittest.TestCase):
         assert data['sells'][0] == {"day": 11, "month": 1, "number": 2000, "price": 44.1, "year": 2007, "securityTitle":"Common Stock", "directOrIndirectOwnership" : "D", "filingURL" : "http://www.sec.gov/Archives/edgar/data/1000180/000124264807000001/0001242648-07-000001-index.htm"}
 
     # # Testing the ability to pull files locally
-    # def test_pull_index_local(self):
-    #     # Give an ftp that is bad.  This will cause a failure if it doesn't pull the correct file
-    #     year = 2008
-    #     quarter = 1
-    #     indexType = 'master'
-    #     fileLoc = 'edgar/full-index/'+str(year)+'/QTR'+str(quarter)+'/'+indexType+'.gz'
-    #     assert os.path.isfile("wsgi/tempFiles/"+fileLoc) #Be sure to include this file in this location for this test to pass
-    #     pull_edgar_file("bad_ftp",fileLoc)
+    def test_pull_index_local(self):
+        # Give an ftp that is bad.  This will cause a failure if it doesn't pull the correct file
+        year = 2008
+        quarter = 1
+        indexType = 'master'
+        fileLoc = 'edgar/full-index/'+str(year)+'/QTR'+str(quarter)+'/'+indexType+'.gz'
+        assert os.path.isfile("wsgi/tempFiles/"+fileLoc) #Be sure to include this file in this location for this test to pass
+        pull_edgar_file("bad_ftp",fileLoc)
 
-    # def test_compute(self):
-    #     # note that the inputs must have /unique/ correct outputs or else
-    #     # the test is meaningless
-    #     inputFile = open('wsgi/testing/computetest.txt', 'r+')
-    #     testDicts = json.load(inputFile)
-    #     for test in testDicts:
-    #         computeResult = json.loads(self.app.post('/compute', content_type='application/json', data=json.dumps(test['input'])).get_data())
-    #         greedyResult = json.loads(self.app.post('/greedy', content_type='application/json', data=json.dumps(test['input'])).get_data())
-    #         # only check the top-level keys from expected output
-    #         for (key, expected) in test['output_compute'].iteritems():
-    #             assert computeResult.get(key) == expected
-    #         for (key, expected) in test['output_greedy'].iteritems():
-    #             assert greedyResult.get(key) == expected
+    def test_compute(self):
+        # note that the inputs must have /unique/ correct outputs or else
+        # the test is meaningless
+        inputFile = open('wsgi/testing/computetest.txt', 'r+')
+        testDicts = json.load(inputFile)
+        for test in testDicts:
+            computeResult = json.loads(self.app.post('/compute', content_type='application/json', data=json.dumps(test['input'])).get_data())
+            greedyResult = json.loads(self.app.post('/greedy', content_type='application/json', data=json.dumps(test['input'])).get_data())
+            # only check the top-level keys from expected output
+            for (key, expected) in test['output_compute'].iteritems():
+                assert computeResult.get(key) == expected
+            for (key, expected) in test['output_greedy'].iteritems():
+                assert greedyResult.get(key) == expected
 
 if __name__ == '__main__':
     unittest.main()
