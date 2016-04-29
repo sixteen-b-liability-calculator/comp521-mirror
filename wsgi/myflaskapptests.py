d178e93cf02b464e6146f9d32df86d6a1dbad047
Alexandra Harrison <apharri3@live.unc.edu>
2016-04-29 00:17:41 -0400

    added test

--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -12,6 +12,11 @@ class FlaskrTestCase(unittest.TestCase):
     def test_blank(self):
         assert 1==1
 
+    def test_parse(self):
+        inputFile = open('wsgi/testing/edgarTestingFile.txt', 'r+')
+        parse = json.load(inputFile)
+        assert not isinstance(parse, basestring)
+
     def test_parse_section_4(self):
 
     	expectedSell = dict(price = 44.10, month = 1, number = 2000, day = 11, year = 2007,
