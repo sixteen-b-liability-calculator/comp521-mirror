e58fbabf2627b3f081324b95b0865d330cd019b9
Alexandra Harrison <apharri3@live.unc.edu>
2016-04-28 21:59:41 -0400

    added the test_compute back in

--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -77,15 +77,14 @@ class FlaskrTestCase(unittest.TestCase):
         testDicts = json.load(inputFile)
         #print testDicts
         for test in testDicts:
-            print (json.dumps(test['input']).get_data())
-            # computeResult = json.loads(self.app.post('/compute', content_type='application/json', data=json.dumps(test['input'])).get_data())
-            # print "This is computeResult" + computeResult
-            # greedyResult = json.loads(self.app.post('/greedy', content_type='application/json', data=json.dumps(test['input'])).get_data())
-            # # only check the top-level keys from expected output
-            # for (key, expected) in test['output_compute'].iteritems():
-            #     assert computeResult.get(key) == expected
-            # for (key, expected) in test['output_greedy'].iteritems():
-            #     assert greedyResult.get(key) == expected
+            computeResult = json.loads(self.app.post('/compute', content_type='application/json', data=json.dumps(test['input'])).get_data())
+            print "This is computeResult" + computeResult
+            greedyResult = json.loads(self.app.post('/greedy', content_type='application/json', data=json.dumps(test['input'])).get_data())
+            # only check the top-level keys from expected output
+            for (key, expected) in test['output_compute'].iteritems():
+                assert computeResult.get(key) == expected
+            for (key, expected) in test['output_greedy'].iteritems():
+                assert greedyResult.get(key) == expected
 
 if __name__ == '__main__':
     unittest.main()
