e35ff720fcdffea4bf52a6fff783615fc8331c26
Alexandra Harrison <apharri3@live.unc.edu>
2016-04-29 12:38:32 -0400

    changed to dict

--- wsgi/myflaskapptests.py
+++ wsgi/myflaskapptests.py
@@ -15,10 +15,8 @@ class FlaskrTestCase(unittest.TestCase):
         assert 1==1
 
     def test_introduces_liability(self):
-        sell = []
-        sell['price'] = 20
-        buy = []
-        buy['price'] = 10
+        sell = {'price': 20}
+        buy = {'price': 10}
         test = introduces_liability(buy, sell, "", "")
         assert test == True 
 
